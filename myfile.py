e3feb4be7c5cbcbcbcdb22943b1638ad8fbb1bc1
jmtotah <jtotah@msudenver.edu>
2019-02-20 19:50:11 -0700

    Working scripts

---
+++ myfile.py
@@ -0,0 +1,65 @@
+print(7 - 4)
+
+string = 'happy'
+d = ['help']
+d = 'help'
+d = {}
+d[0] = 'no'
+d[1] = 'yesterday'
+d['mana'] = 'mexican pop group'
+print(d.keys())
+d.keys()
+d.keys()
+type(d)
+for i in range(10):
+ print(i)
+
+for t in range(32):
+ print(t)
+l = [4,5,6]
+for i in l:print(i)
+u = ['interesting']
+for i in u:
+    print(i)
+i = 8
+while i > 0:
+    print(i)
+    i = i - 1
+    i -= 1
+y = 4
+while y <= 9:
+    print(y)
+    y += 1
+i = 8
+while True:
+    print(" ")
+    print(i)
+    if i <=0:
+        break
+    i -= 1
+p = 12
+while True:
+    print(p)
+    if p < 7:
+        break
+    p -= 1
+i = 8
+if i > 0:
+    print("i is greater than zero")
+else:
+    print("i is less than zero")
+h = 20
+if h >= 21:
+    print("loser")
+else:
+    print("winner")
+
+g = 13
+while g < 18:
+    if g < 17:
+        print('Im good')
+    else:
+        print("Im not good")
+
+    g = g + 1
+
