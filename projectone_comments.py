93562c893a83ac285880740cf73d9360a0bdd211
jmtotah <jtotah@msudenver.edu>
2019-02-23 20:20:05 -0700

    Sorting algorithms questions under 1

---
+++ projectone_comments.py
@@ -0,0 +1,13 @@
+#Sorting Algorithms
+#1.
+ #i. len, range, print .bubble_sort
+ #ii. Only bubble_sort is defined in the program. Len, range and print are built in.
+ #iii. 2 loops. The first loop is nested in  bubble_sort and the second loop in nested in the first
+ #iv. There is 1 conditional statement and it is nested in the second loop.
+ #v. 2 levels.
+ #vi. Yes, the array. In this case, it has to be a list.
+ #vii. On the 24th line by typing bubble_sort
+ #viii. It’s a feature of Python
+ #ix. Yes, it is underlined. It refers to the fact that arr is in the outer scope
+ #x. Yes, it is underlined. It refers to the fact that i is in the outer scope
+ #xi. No
